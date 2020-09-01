e1b8eb3185cc11b45023a314216ef7ab30ed54fb
redrover9 <grace.thompson97@gmail.com>
2020-09-01 08:25:18 -0700

    Create dubstep.py

---
+++ dubstep.py
@@ -0,0 +1,9 @@
+def song_decoder(song):
+    song_list = song.split("WUB")
+    song_list = ' '.join(song_list)
+    song_list = song_list.strip()
+    song_list = ' '.join(song_list.split())
+    return song_list
+
+
+print(song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"))
